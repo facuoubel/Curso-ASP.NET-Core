19e17ab4ed39d59a70629bb286ec32e199e0a48b
Facundo Oubel <FacundoOubel@hotmail.com>
2022-03-02 20:20:03 -0300

    Punto cero

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Curso_ASP.NET_Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
